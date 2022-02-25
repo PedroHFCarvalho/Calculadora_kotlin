23df5feca2b0dc64e1a99006b80785f622bbe292
Pedro Carvalho <ph.carvalho2612@gmail.com>
2022-02-25 12:55:40 -0300

    Ataulizacao do menu

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -1,3 +1,5 @@
+import kotlin.math.pow
+
 class Calculator {
 
     public fun menu() {
@@ -6,24 +8,21 @@ class Calculator {
         println("Digite * {Multplicação}")
         println("Digite / {Divisão}")
         println("Digite % {Resto}")
+        println("Digite ** {Potencia}")
     }
 
     public fun operation(opera: String, num1: Int, num2: Int): String {
 
-        // Identifica operador
-        if (opera.compareTo("+", true) == 0) {
-            return soma(num1, num2)// chama calculo com retorno em texto
-        } else if (opera.compareTo("-", true) == 0) {
-            return subtract(num1, num2)// chama calculo com retorno em texto
-        } else if (opera.compareTo("*", true) == 0) {
-            return multiplication(num1, num2)// chama calculo com retorno em texto
-        } else if (opera.compareTo("/", true) == 0) {
-            return divisor(num1, num2)// chama calculo com retorno em texto
-        } else if (opera.compareTo("%", true) == 0) {
-            return rests(num1, num2)// chama calculo com retorno em texto
-        } else {
-            return "nao foi possivel identificar a operaçao" // msg de erro
+        return when (opera) {
+            "+" -> soma(num1, num2)// chama calculo com retorno em texto
+            "-" -> subtract(num1, num2)// chama calculo com retorno em texto
+            "*" -> multiplication(num1, num2)// chama calculo com retorno em texto
+            "/" -> divisor(num1, num2)// chama calculo com retorno em texto
+            "%" -> rests(num1, num2)// chama calculo com retorno em texto
+            "**" -> potencia(num1, num2)// chama calculo com retorno em texto
+            else -> "nao foi possivel identificar a operaçao" // msg de erro
         }
+
     }
 
     private fun soma(num1: Int, num2: Int): String {
@@ -49,6 +48,11 @@ class Calculator {
     private fun rests(num1: Int, num2: Int): String {
         var result = num1 % num2
         return "$num1 % $num2 = $result"
+    }
+
+    private fun potencia(num1: Int, num2: Int): String {
+        var result = num1.toDouble().pow(num2.toDouble())
+        return "$num1 ** $num2 = $result"
     }
 
 
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,22 +2,24 @@ import java.util.Scanner
 
 fun main() {
 
-    val read = Scanner(System.`in`) //Instancia Scanner
     val calc = Calculator() // Instancia Calculadora
     var arr = IntArray(2) // Inicia array com 2 slots
 
     do {
         for (i in 0..1) {
             println("Digite o ${i + 1}° numero: ")
-            arr[i] = read.nextInt()
+            arr[i] = readLine()!!.toInt()
         }
 
         calc.menu() // Chama menu
-        var opera = read.next()
+        var opera = readLine()!!
+
         println(calc.operation(opera, arr[0], arr[1]))
 
         println("deseja calcular novamente? [S/s] ou [N/n]")
-        var yesOrNot = readLine()
+        var yesOrNot = readLine()!!
 
-    } while (yesOrNot == "s" || yesOrNot == "S") // repetiçao do programa
+    } while (yesOrNot.compareTo("s", true) == 0) // repetiçao do programa
+
+
 }
